6fe923dd0f7a2eaf7fa02747a3b6cc3bb4887b3c
Ali <b12334566@gmail.com>
2025-07-03 20:11:12 +0300

    Implemented the following features: reading the excel file, extracting all the information necessary for the summary report, writing the report into the subnet_report.csv file.

---
+++ subnet_analyzer.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import ipaddress
+
+def main():
+    ip_df = pd.read_excel('ip_data.xlsx', 'Sheet1')
+
+    # Dictionary that contains the necessary information that will be written into the CSV file.
+    summary_report = {
+        'IP Address' : [],
+        'Subnet Mask' : [],
+        'CIDR Notation' : [],
+        'Network Address' : [],
+        'Broadcast Address' : [],
+        'Number of Host Addresses' : []
+    }
+
+    for ip, mask in zip(ip_df['IP Address'], ip_df['Subnet Mask']):
+        ip_data = ipaddress.IPv4Interface((ip, mask))
+        add_to_report(summary_report, ip_data)
+
+    csv_df = pd.DataFrame(summary_report)
+    csv_df.to_csv('subnet_report.csv', index=False)
+
+# Used to add the information from the IPv4Interface to the report dictionary.
+def add_to_report(summary_report, ip_data):
+    summary_report['IP Address'].append(str(ip_data.ip))
+    summary_report['Subnet Mask'].append(str(ip_data.netmask))
+    summary_report['CIDR Notation'].append(str(ip_data))
+    summary_report['Network Address'].append(str(ip_data.network.network_address))
+    summary_report['Broadcast Address'].append(str(ip_data.network.broadcast_address))
+    summary_report['Number of Host Addresses'].append(ip_data.network.num_addresses - 2)
+
+if __name__ == '__main__':
+    main()
